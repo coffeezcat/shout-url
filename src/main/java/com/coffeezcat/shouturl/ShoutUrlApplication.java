f7619d4f1930e9ad0c97aa835cd8b4711ce63646
yangweia <yangweia@zbj.com>
2019-04-16 17:44:33 +0800

    项目初始化

---
+++ src/main/java/com/coffeezcat/shouturl/ShoutUrlApplication.java
@@ -0,0 +1,13 @@
+package com.coffeezcat.shouturl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoutUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoutUrlApplication.class, args);
+	}
+
+}
